2add5ce916e0a8a335fa15f12153cd37fdd94aa1
Eric Oliveira <oiericoi@hotmail.com>
2023-09-07 15:27:03 -0300

    feat: add About component

---
+++ src/components/About/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+const About = () => {
+  return (
+    <div className="about">
+      <p id="about-paragraph">Bem-vindo a um lugar onde a jornada da autodescoberta e do desenvolvimento pessoal é a prioridade absoluta. No Espaço de Atendimento Neuro, Psico e Pedagógico (EANPS), acreditamos que todos têm o potencial de crescer, superar desafios e alcançar seus objetivos, independentemente das barreiras que possam enfrentar.</p>
+      <img id="about-img" src="/images/free.svg" alt="" />
+    </div>
+  )
+}
+
+export default About
