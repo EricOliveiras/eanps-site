3af7a25ca88ae8081f123960f77bfcad5af57e6b
Eric Oliveira <oiericoi@hotmail.com>
2023-09-07 15:27:51 -0300

    refactor: modify App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import './index.css'
+import Header from './components/Header'
+import Banner from './components/Banner'
+import About from './components/About'
 
 function App() {
-
+  return (
+    <>
+      <Header />
+      <Banner />
+      <About />
+    </>
+  )
 }
 
 export default App
