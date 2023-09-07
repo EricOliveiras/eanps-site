ad8a19d3e4ae27578832f51d5c5aca1bac4e20bd
Eric Oliveira <oiericoi@hotmail.com>
2023-09-07 15:26:25 -0300

    feat: add Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import './style.css'
+
+const Header = () => {
+    return (
+      <header id="header">
+        <h1 id="header-title"><a href="#">EANPS</a></h1>
+        <nav>
+          <ul>
+            <li><a href="/">Início</a></li>
+            <li><a href="/sobre">Sobre</a></li>
+            <li><a href="/contato">Contato</a></li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
+  
+  export default Header
+  
