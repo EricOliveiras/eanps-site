fb22c39741aad8856818de67cd87f591ac65cf67
Eric Oliveira <oiericoi@hotmail.com>
2023-09-07 15:27:17 -0300

    feat: add Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+  return (
+    <p>
+      <a href="https://br.freepik.com/vetores-gratis/ilustracao-do-conceito-de-psicologo_8426451.htm#query=psicologia&from_query=pscologia&position=6&from_view=search&track=sph">Imagem de storyset</a> no Freepik
+      <a href="https://br.freepik.com/vetores-gratis/sentindo-se-livre-ilustracao-do-conceito_34793468.htm#page=3&position=41&from_view=author">Imagem de storyset</a> no Freepik
+    </p>
+  )
+}
+
+export default Footer
