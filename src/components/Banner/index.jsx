62cb119e3f464385afd29eb227144ac506a5c6ee
Eric Oliveira <oiericoi@hotmail.com>
2023-09-07 15:26:49 -0300

    feat: add Banner component

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <img id="banner-img" src="/images/banner.svg" alt="" />
+      <p id="banner-paragraph">Descubra o poder da transformação em nosso Espaço de Atendimento Neuro, Psico e Pedagógico, onde cada desafio se torna uma oportunidade e cada passo é um caminho para o seu sucesso.</p>
+    </div>
+  )
+}
+
+export default Banner
