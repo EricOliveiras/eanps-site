6020527e763bf5b6731df73d37fda6fbe6b2c783
Eric Oliveira <oiericoi@hotmail.com>
2023-09-09 13:06:23 -0300

    refactor: modify banner component

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,10 +2,10 @@ import './style.css'
 
 const Banner = () => {
   return (
-    <div className="banner">
+    <section id="banner" className="banner">
       <img id="banner-img" src="/images/banner.svg" alt="" />
       <p id="banner-paragraph">Descubra o poder da transformação em nosso Espaço de Atendimento Neuro, Psico e Pedagógico, onde cada desafio se torna uma oportunidade e cada passo é um caminho para o seu sucesso.</p>
-    </div>
+    </section>
   )
 }
 
