3441860e005c0e5bef2160fbbbabb522371a5407
Eric Oliveira <oiericoi@hotmail.com>
2023-09-10 15:10:07 -0300

    refactor: modify footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,15 @@
+import './style.css'
 
 const Footer = () => {
   return (
-    <p>
-      <a href="https://br.freepik.com/vetores-gratis/ilustracao-do-conceito-de-psicologo_8426451.htm#query=psicologia&from_query=pscologia&position=6&from_view=search&track=sph">Imagem de storyset</a> no Freepik
-      <a href="https://br.freepik.com/vetores-gratis/sentindo-se-livre-ilustracao-do-conceito_34793468.htm#page=3&position=41&from_view=author">Imagem de storyset</a> no Freepik
-      <a href="https://br.freepik.com/vetores-gratis/ilustracao-do-conceito-de-classificacao-de-pensamentos_12780668.htm#query=psico&position=42&from_view=author">Imagem de storyset</a> no Freepik
-      <a href="https://br.freepik.com/vetores-gratis/ilustracao-do-conceito-de-crescimento-pessoal_30590322.htm#query=psico&position=46&from_view=author">Imagem de storyset</a> no Freepik
-      <a href="https://br.freepik.com/vetores-gratis/ilustracao-do-conceito-de-orgao-cerebral_32318399.htm#query=psico&position=30&from_view=author">Imagem de storyset</a> no Freepik
-      <a href="https://br.freepik.com/vetores-gratis/ilustracao-do-conceito-de-imaginacao_20824337.htm#query=psico&position=33&from_view=author">Imagem de storyset</a> no Freepik
-    </p>
+    <footer id="footer">
+      <p>
+        Desenvolvido por <a href="https://github.com/EricOliveiras">Eric Oliveira</a> - 2023
+      </p>
+      <p>
+        <a href="https://br.freepik.com/autor/stories">Imagens de storyset</a> no Freepik
+      </p>
+    </footer>
   )
 }
 
