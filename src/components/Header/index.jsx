d290eeb1c138c42cd163648fb52bb2e4987c5a40
Eric Oliveira <oiericoi@hotmail.com>
2023-09-10 15:09:29 -0300

    refactor: modify header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,14 +7,6 @@ const Header = () => {
           <h1 id="header-title"><a href="/">EANPS</a></h1>
           <p id="header-paragraph">ESPAÇO DE ATENDIMENTO NEURO, PSICO E PEDAGÓGICO</p>
         </div>
-        <nav>
-          <ul>
-            <li><a href="/">Início</a></li>
-            <li><a href="#about">Sobre</a></li>
-            <li><a href="#services">Serviços</a></li>
-            <li><a href="#contato">Contato</a></li>
-          </ul>
-        </nav>
       </header>
     )
   }
